30645fdeb1a00dddbda849eff71dcd0a2ba71ed1
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-09-22 18:03:19 +0100

    connect to mongoDB and keystoneJS

---
+++ backend/.keystone/schema-types.ts
@@ -0,0 +1,30 @@
+type Scalars = {
+  readonly ID: string;
+  readonly Boolean: boolean;
+  readonly String: string;
+  readonly Int: number;
+  readonly Float: number;
+  readonly JSON: import('@keystone-next/types').JSONValue;
+};
+
+export type _ksListsMetaInput = {
+  readonly key?: Scalars['String'] | null;
+  readonly auxiliary?: Scalars['Boolean'] | null;
+};
+
+export type _ListSchemaFieldsInput = {
+  readonly type?: Scalars['String'] | null;
+};
+
+export type KeystoneAdminUIFieldMetaCreateViewFieldMode = 'edit' | 'hidden';
+
+export type KeystoneAdminUIFieldMetaListViewFieldMode = 'read' | 'hidden';
+
+export type KeystoneAdminUIFieldMetaItemViewFieldMode =
+  | 'edit'
+  | 'read'
+  | 'hidden';
+
+export type KeystoneAdminUISortDirection = 'ASC' | 'DESC';
+
+export type KeystoneListsTypeInfo = {};
